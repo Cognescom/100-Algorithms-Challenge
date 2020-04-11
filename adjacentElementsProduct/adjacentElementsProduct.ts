736a318a35a81323b34a06d58507800d5102b2b0
Stas Neteruk <cognes.com@gmail.com>
2020-04-11 23:58:36 +0300

    Solved adjacentElementProducts

---
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -0,0 +1,10 @@
+function adjacentElementsProduct(inputArray: number[]): number {
+  let maxAdjacentProduct = inputArray[0] * inputArray[1];
+  for (let i = 1; i < inputArray.length - 1; i++) {
+    let product = inputArray[i] * inputArray[i+1];
+    if(product > maxAdjacentProduct) maxAdjacentProduct = product;    
+  }
+  return maxAdjacentProduct;
+}
+
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
